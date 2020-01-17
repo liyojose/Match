0fda46828c5146b6c536987fc5697363cba2e5e4
Liyo Jose <Liyo.Jose@gds.ey.com>
2020-01-17 14:22:18 +0530

    Initial Commit

---
+++ Match.API.V1/Match.API.V1/Controllers/ValuesController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Match.API.V1.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Match.API.V1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _dataContext; 
+
+
+        public ValuesController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        // GET: api/Values
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_dataContext.Values.ToList());
+        }
+
+        // GET: api/Values/5
+        [HttpGet("{id}", Name = "Get")]
+        public IActionResult Get(int id)
+        {
+            return Ok(_dataContext.Values.SingleOrDefault(v=> v.Id == id));
+        }
+
+        // POST: api/Values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Match.API.V1/Match.API.V1/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Match.API.V1.Data
+{
+    public partial class DataContext : DbContext
+    {
+        public DataContext()
+        {
+        }
+
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Value> Values { get; set; }
+    }
+    }
